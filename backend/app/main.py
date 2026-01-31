2a5e7468813992ba4a0445316967108bfdc94c9c
Nimisha negi <nimishanegi2003@gmail.com>
2026-01-31 23:20:12 +0530

    Fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,6 +11,7 @@ app = FastAPI(title="Daruka.Earth Backend", version="1.0")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://darukaa.onrender.com"
 ]
 
 app.add_middleware(
